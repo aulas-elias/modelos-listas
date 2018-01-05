dcf1062feaf9fb110e8a0190dc10a288bb0ff68a
Elias Alves <unixelias@gmail.com>
2018-01-05 00:39:16 -0200

    Adiciona modelos básicos

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # modelos-listas
 Template para entrega das Listas 1 e 2
+
+Em breve mais instruções
---
+++ src/exemplo.sql
@@ -0,0 +1,36 @@
+/*
+ Esse é um texto de exemplo
+ Você pode fazer um arquivo para cada lista, identificando nos comentário o exercício demonstrado.
+ Por exemplo:
+ Você pode apagar esse texto
+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+*/
+-- Nome: Fulano de Tal
+-- Matrícula: 20101011202
+-- Lista 1
+
+
+-- Exercício 1.1-b 
+-- Query que .....
+
+SELECT nome_cd, preco_venda, CASE
+	WHEN preco_venda < 10 THEN
+	preco_venda * 0.9
+	WHEN preco_venda >=10 AND preco_venda < 13 THEN
+	preco_venda * .8
+	ELSE
+	preco_venda * .7
+	END venda
+FROM cd ;
+
+
+-- Exercícios da Lista 1
+-- Exercício 1.1-d
+-- Query que .....
+
+CREATE VIEW salario_por_departamento
+	AS SELECT nome_depto, sum (salario) AS
+		salario total
+	FROM funcionario f JOIN departamento d ON (f.
+	cod_depto = d.cod_depto )
+	GROUP by nome_depto ;
